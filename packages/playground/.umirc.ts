796d01b3def8af118168d21b88412d88e7a6f232
hayden <hayden9653@gmail.com>
2023-03-26 15:11:39 +0800

    feat: aaa

--- packages/playground/.umirc.ts
+++ packages/playground/.umirc.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     // webpack 会在静态文件路径前面添加 publicPath, 在html中可以看到 /umi.js 变成了 ./umi.js，
     //  此时部署到github page 上不会导致请求 umi.js 文件 404
     publicPath: './',
-    // 设置路由前缀，通常用于部署到非根目录, 设置后改变 window.routerBase
+    // 设置路由前缀，通常用于部署到非根目录, 设置后改变 window.routerBase 的指向
     // 此时部署到 github page 上才不会白屏
     base: '/dataSync-form-renderer/',
 });
